a3baafadeeb075dee9e6c1f77ce6aa59092e0568
Jamie Sheddan <jsheddan@outlook.com>
2023-11-27 15:50:48 +0000

    uncompleted models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/edit'
+  get 'bookings/update'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -6,7 +10,14 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  resources :locations, only: %i[index show] do
+    resources :bookings, only: %i[new create]
+  end
 
+  resources :users, only: %i[show] do
+    resources :bookings, only: %i[edit update]
+  end
   # Defines the root path route ("/")
   # root "posts#index"
+  get "users/:id", to: "users#show", as: "dashboard"
 end
