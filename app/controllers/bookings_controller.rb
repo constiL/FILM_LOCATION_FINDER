e65ab82c2d331c9fe76cdfddee0218bf025cbda4
Jamie Sheddan <jsheddan@outlook.com>
2023-11-28 14:09:03 +0000

    locations complete, bookings almost

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,15 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to 
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -3,15 +3,26 @@ class LocationsController < ApplicationController
     @locations = Location.all
   end
 
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      redirect_to location_path(@location)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
     @location = Location.find(params[:id])
   end
 
-  def new
+  private
+
+  def location_params
+    params.require(:location).permit(:name, :city, :description, :style, :rooms, :outdoor_space, :image_url)
   end
-
-  def create
-  end
-
-
 end
