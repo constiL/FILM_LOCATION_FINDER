e0e697e8432aa9be6b3528ec1a92e78a71e4c58e
Jamie Sheddan <jsheddan@outlook.com>
2023-11-28 13:15:11 +0000

    city column added to location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_114516) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_28_131306) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_114516) do
     t.string "name"
     t.string "image_url"
     t.text "description"
+    t.string "city"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
