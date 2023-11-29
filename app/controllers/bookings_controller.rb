718005961c475e3643d3070ff298e9634257777c
Constantin Lerner <colerner@me.com>
2023-11-29 11:58:38 +0100

    booking edit & update complete but not tested

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,8 +6,17 @@ class BookingsController < ApplicationController
   end
 
   def edit
+    @booking = Booking.find(params[:id])
   end
 
   def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: "Booking was succesfully updated", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
   end
+
+  private
+
 end
