ff80f6606e82af5c09abc164b962fbe34ec8ffed
Constantin Lerner <colerner@me.com>
2023-11-29 12:48:29 +0100

    added edit & update and changed new & create to the bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,14 @@
 class BookingsController < ApplicationController
   def new
+    @location = Location.find(params[:location_id])
     @booking = Booking.new
   end
 
   def create
     @booking = Booking.new(booking_params)
+    @location = Location.find(params[:location_id])
+    @booking.location = @location
+    @booking.user = current_user
     if @booking.save
       redirect_to locations_path
     else
@@ -18,10 +22,12 @@ class BookingsController < ApplicationController
 
   def update
     @booking = Booking.find(params[:id])
+    @booking.user = current_user
     if @booking.update(booking_params)
       redirect_to @booking, notice: "Booking was succesfully updated", status: :see_other
     else
       render :edit, status: :unprocessable_entity
+    end
   end
 
   private
