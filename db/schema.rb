349687812aff34bb3a15f67811c871ed67edf788
Constantin Lerner <colerner@me.com>
2023-11-29 15:21:22 +0100

    seeding is done, picture abit whacky

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_131306) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_130459) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_131306) do
     t.string "image_url"
     t.text "description"
     t.string "city"
+    t.string "address"
     t.index ["user_id"], name: "index_locations_on_user_id"
   end
 
