a18660bf27f851fecec82d70df6bf766d6a7cdf9
Thomas Furio <tommyfurio@gmail.com>
2023-11-29 14:38:15 +0100

    map-location branch

--- app/models/location.rb
+++ app/models/location.rb
@@ -8,4 +8,7 @@ class Location < ApplicationRecord
   validates :style, inclusion: { in: %w[modern minimalistic rustic chic mediterranean] }
   validates :rooms, numericality: { greater_than_or_equal_to: 1 }
   validates :city, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
